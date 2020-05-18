2fdb705a15b8544bdbfbede33ea84ef9aa3223c6
z451343010 <1178214954@qq.com>
2020-05-19 05:28:11 +0800

    continue课堂练习修改

--- conditionalControl/continueTest/continueExc2/main.go
+++ conditionalControl/continueTest/continueExc2/main.go
@@ -17,19 +17,22 @@ func main() {
 		fmt.Println("请输入数字")
 		fmt.Scanln(&inputNum)
 
-		if inputNum > 0 {
-			positiveNum++
-			continue
-		}
-
-		if inputNum < 0 {
-			negativeNum++
-			continue
-		}
-
 		if inputNum == 0 {
 			break
 		}
+
+		if inputNum > 0 {
+			positiveNum++
+			// 当 inputNum > 0 时，往下执行已经无意义
+			// 因此可以用continue跳出循环
+			// 不再执行下面的语句
+			continue
+		}
+
+		// 只有当输入的数字为负数时，才能执行到这条语句
+		// 因为输入的数字为0时已经跳出循环
+		// 输入的数字为正数的时候，跳过本次循环
+		negativeNum++
 
 	}
 
