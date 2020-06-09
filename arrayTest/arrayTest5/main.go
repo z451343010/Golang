239be2cb9e61b9c93f9a37b204e52baaea8859b8
z451343010 <1178214954@qq.com>
2020-06-10 04:29:53 +0800

    数组细节

--- arrayTest/arrayTest5/main.go
+++ arrayTest/arrayTest5/main.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
-// Go的数组属于值类型。在默认情况下是值传递，因此会进行值拷贝。
-// 数组间不会相互影响。（在很多其它编程语言中，数组是引用类型）
-func test(testArr [3]int) {
-	testArr[0] = 88
+// 如果想在其它函数中
+// 去修改原来的数组，可以使用 （指针方式）
+func test(testArr *[3]int) {
+	(*testArr)[0] = 88
 }
 
 func main() {
 	var arr = [3]int{11, 22, 33}
+	test(&arr)
 	fmt.Println(arr)
+	fmt.Printf("%p\n", &arr)
+	var ptr *[3]int = &arr
+	fmt.Println(&ptr)
+	fmt.Println(&arr[0])
+	// fmt.Println(&arr == &arr[0])
 }
