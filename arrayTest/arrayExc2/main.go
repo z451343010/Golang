b43f8ab0971379ee9349e6b2ae5038d678800e0a
z451343010 <1178214954@qq.com>
2020-06-11 04:13:23 +0800

    数组练习题修改

--- arrayTest/arrayExc2/main.go
+++ arrayTest/arrayExc2/main.go
@@ -5,15 +5,16 @@ import "fmt"
 // 请求出一个数组的最大值，并得到对应的下标
 func main() {
 
-	var numArr = [5]float64{9.9, 3.5, 110.72, 3.3, 56}
-	var arrMaxNum float64
+	var numArr = [5]float64{-9.9, -3.5, -110.72, -3.3, -56}
+	var arrMaxNum float64 = numArr[0]
 	var arrMaxIndex int
 
-	for index, value := range numArr {
-		if value > arrMaxNum {
-			arrMaxNum = value
-			arrMaxIndex = index
+	for i := 1; i < len(numArr); i++ {
+		if arrMaxNum < numArr[i] {
+			arrMaxNum = numArr[i]
+			arrMaxIndex = i
 		}
+
 	}
 
 	fmt.Println(arrMaxNum)
