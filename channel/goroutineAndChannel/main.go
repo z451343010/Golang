2fa74b1b8a9000ae3e8edd5528d21ed7f1e7f108
z451343010 <1178214954@qq.com>
2020-10-07 10:12:41 +0800

    go协程和管道channel

--- channel/goroutineAndChannel/main.go
+++ channel/goroutineAndChannel/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 	goroutine和channel结合应用实例
 	请完成goroutine和channel协同工作的案例，具体要求：
@@ -8,6 +10,49 @@ package main
 	3）注意：writeData和readData操作的是同一个管道。
 	4）主线程需要等待writeData和readData协程都完成工作才能退出
 */
+
+// write Data
+func writeData(intChan chan int) {
+	for i := 0; i < 50; i++ {
+		intChan <- i
+		fmt.Println(" ====== writeData working ======")
+	}
+	// 数据写完进管道，即可关闭
+	close(intChan)
+}
+
+// read Data
+func readData(intChan chan int, exitChan chan bool) {
+
+	for {
+		v, ok := <-intChan
+		if !ok {
+			break
+		}
+		fmt.Println(" ====== readData working ======")
+		fmt.Println("readData read data = ", v)
+	}
+
+	// readData 读取完数据以后，即任务完成
+	exitChan <- true
+	close(exitChan)
+
+}
+
 func main() {
+
+	// 创建两个管道
+	intChan := make(chan int, 50)
+	exitChan := make(chan bool, 1)
+
+	go writeData(intChan)
+	go readData(intChan, exitChan)
+
+	for {
+		_, ok := <-exitChan
+		if !ok {
+			break
+		}
+	}
 
 }
