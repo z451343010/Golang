96d19ec99e95422da54a74085b118b4d908a1079
z451343010 <1178214954@qq.com>
2020-12-29 03:07:01 +0800

    数据结构 链表 哈希表

--- dataStruct/hashtable/test1/main.go
+++ dataStruct/hashtable/test1/main.go
@@ -4,7 +4,10 @@
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // 雇员
 type Emp struct {
@@ -23,39 +26,40 @@ type EmpLink struct {
 // 保证添加时，编号是从小到大
 func (this *EmpLink) InsertEmp(emp *Emp) {
 
-	cur := this.Head   // 辅助指针
-	var pre *Emp = nil // 辅助指针 pre 在 cur 前面
+	cur := this.Head // 辅助指针
 
 	// 如果当前的 EmpLink 是一个空链表
-	if cur == nil {
-		this.Head = emp
+	if cur.Next == nil {
+		cur.Next = emp
 		return
 	}
 
-	// 如果不是一个空链表，需要给 emp 找到对应的位置插入
-	// 让 cur 和 emp 比较，让 pre 始终保持在 cur 前面
+	flag := true
+
 	for {
 
-		if cur != nil {
-			if cur.Id > emp.Id {
-				// 找到了插入的位置
-				break
-			}
-			// 保证 pre 始终在 cur 前面
-			pre = cur
-			cur = cur.Next
-		} else {
+		if cur.Next == nil {
 			break
 		}
 
+		if cur.Next.Id == emp.Id {
+			flag = false
+			break
+		}
+
+		if cur.Next.Id > emp.Id {
+			break
+		}
+
+		cur = cur.Next
+
 	}
 
-	if cur == nil { // 将 emp 添加到链表的最后
-		pre.Next = emp
-		emp.Next = nil
+	if flag {
+		emp.Next = cur.Next
+		cur.Next = emp
 	} else {
-		pre.Next = emp
-		emp.Next = cur
+		fmt.Printf("已经有了编号为 %d 的员工\n", emp.Id)
 	}
 
 }
@@ -63,13 +67,13 @@ func (this *EmpLink) InsertEmp(emp *Emp) {
 // 显示当前链表的信息
 func (this *EmpLink) ShowLink(no int) {
 
-	if this.Head == nil {
+	if this.Head == nil || this.Head.Next == nil {
 		fmt.Printf("当前链表 %d 为空", no)
 		return
 	}
 
 	// 遍历当前的链表并显示数据
-	cur := this.Head // 辅助节点
+	cur := this.Head.Next // 辅助节点
 	for cur != nil {
 		fmt.Printf("链表[%d] 雇员id = %d 名字 = %s -> ", no, cur.Id, cur.Name)
 		cur = cur.Next
@@ -98,7 +102,7 @@ func (this *HashTable) Insert(emp *Emp) {
 
 }
 
-// 编写一个方法，完成查找某个雇员
+// 查找某个雇员
 func (this *HashTable) FindEmp(id int) {
 
 	linkNo := this.HashFunc(id)
@@ -122,7 +126,40 @@ func (this *HashTable) FindEmp(id int) {
 
 }
 
-// 编写方法，显示 Hash table 的所有雇员
+// 删除某个雇员
+func (this *HashTable) DelNodeById(id int) {
+
+	linkNo := this.HashFunc(id)
+	temp := this.LinkArr[linkNo].Head
+	flag := false
+
+	// 如果对应的链表为空
+	if temp.Next == nil {
+		fmt.Println("该链表为空")
+		return
+	}
+
+	for temp != nil {
+
+		if temp.Next.Id == id {
+			flag = true
+			break
+		}
+
+		temp = temp.Next
+
+	}
+
+	if flag {
+		fmt.Printf("节点 %d 已经被删除\n", id)
+		temp.Next = temp.Next.Next
+	} else {
+		fmt.Printf("未找到节点 %d", id)
+	}
+
+}
+
+// 显示 Hash table 的所有雇员
 func (this *HashTable) ShowAllLink() {
 
 	for i := 0; i < len(this.LinkArr); i++ {
@@ -137,13 +174,20 @@ func main() {
 	key := ""
 	id := 0
 	name := ""
-	var hashTable HashTable
+	hashTable := &HashTable{}
+
+	// 初始化 hashTable.LinkArr 里的 head 节点
+	for i := 0; i < len(hashTable.LinkArr); i++ {
+		head := &Emp{}
+		hashTable.LinkArr[i].Head = head
+	}
 
 	for {
 		fmt.Println("\n============雇员系统菜单============")
 		fmt.Println("input 表示添加雇员")
 		fmt.Println("show 表示显示雇员")
 		fmt.Println("find 表示查找雇员")
+		fmt.Println("delete 表示删除雇员")
 		fmt.Println("exit 表示退出系统")
 
 		fmt.Scanln(&key)
@@ -164,8 +208,12 @@ func main() {
 			fmt.Println("请输入id号：")
 			fmt.Scanln(&id)
 			hashTable.FindEmp(id)
+		case "delete":
+			fmt.Println("请输入要删除的节点id：")
+			fmt.Scanln(&id)
+			hashTable.DelNodeById(id)
 		case "exit":
-
+			os.Exit(0)
 		}
 
 	}
