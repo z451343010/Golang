2db53f1f2ea7fb666112889dd943cd9289006ba9
z451343010 <1178214954@qq.com>
2021-01-10 16:50:32 +0800

    Gin框架 结构体绑定form ShouldBindQuery

---
+++ GinDemo/ginBlindData/ShouldBindQuery/main.go
@@ -0,0 +1,46 @@
+/*
+	Gin 框架
+	表单绑定结构体
+	ShouldBlindQuery
+*/
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 创建映射的结构体
+type Student struct {
+	Name    string `form:"name"`
+	Classes string `form:"classes"`
+}
+
+func main() {
+
+	engine := gin.Default()
+
+	// http://localhost:8080/hello?name=Davie&classes=软件工程
+	engine.GET("/hello", func(context *gin.Context) {
+
+		fmt.Println(context.FullPath())
+
+		var student Student
+		err := context.ShouldBindQuery(&student)
+
+		if err != nil {
+			return
+		}
+
+		fmt.Println(student.Name)
+		fmt.Println(student.Classes)
+
+		context.Writer.Write([]byte("hello " +
+			student.Name + " " + student.Classes + "\n"))
+
+	})
+
+	engine.Run()
+
+}
