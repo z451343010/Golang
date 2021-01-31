d3ab70180fc7a43a37d1e0ae6980b5b30cd1ffcb
z451343010 <1178214954@qq.com>
2021-01-31 14:55:03 +0800

    项目：进出货管理系统 提交进货单

--- FinancialSystem/controller/PurchaseOrderDetailController.go
+++ FinancialSystem/controller/PurchaseOrderDetailController.go
@@ -1,4 +1,52 @@
 package controller
 
+import (
+	"fmt"
+	"gin/FinancialSystem/model"
+	"gin/FinancialSystem/service"
+	"gin/FinancialSystem/tool"
+
+	"github.com/gin-gonic/gin"
+)
+
 type PurchaseOrderDetailController struct {
+}
+
+func (podc *PurchaseOrderDetailController) Router(engine *gin.Engine) {
+	engine.POST("/user/commit_purchase_order", podc.commitPurchaseOrder)
+}
+
+// 提交订单
+func (podc *PurchaseOrderDetailController) commitPurchaseOrder(context *gin.Context) {
+
+	// 接收前端传递过来的购物车参数
+	var cartParam model.PurchaseShoppingCart
+	err := tool.Decode(context.Request.Body, &cartParam)
+	if err != nil {
+		tool.Failed(context, "购物车详情，参数解析失败")
+		return
+	}
+
+	// 获取订单ID
+	orderId := cartParam.OrderId
+	purchaseShoppingCartService := service.PurchaseShoppingCartService{}
+	purchaseShoppingCarts := purchaseShoppingCartService.FindPurchaseDetailByOrderId(orderId)
+
+	purchaseDetailOrderId := tool.CreateOrderId()
+	var purchaseOrderDetails []model.PurchaseOrderDetail
+
+	for _, value := range purchaseShoppingCarts {
+		purchaseOrderDetail := model.PurchaseOrderDetail{orderId: purchaseDetailOrderId}
+		purchaseOrderDetail.GoodsId = value.GoodsId
+		purchaseOrderDetail.GoodsUnitNum = value.GoodsUnitNum
+		purchaseOrderDetail.GoodsSingleNum = value.GoodsSingleNum
+		purchaseOrderDetail.GoodsUnitPrice = value.GoodsUnitPrice
+		purchaseOrderDetail.GoodsSinglePrice = value.GoodsSinglePrice
+		purchaseOrderDetail.OrderTotalPrice = value.OrderTotalPrice
+		purchaseOrderDetail.IsDebt = value.IsDebt
+		append(purchaseOrderDetails, purchaseOrderDetail)
+	}
+
+	fmt.Println("purchaseOrderDetails = ", purchaseOrderDetails)
+
 }
--- FinancialSystem/model/PurchaseOrderDetail.go
+++ FinancialSystem/model/PurchaseOrderDetail.go
@@ -4,12 +4,13 @@
 package model
 
 type PurchaseOrderDetail struct {
-	OrderId          int64   `xorm:"pk not null" json:"order_id"`      // 进货单ID
-	GoodsId          int64   `xorm:"pk not null" json:"goods_id"`      // 商品ID
-	GoodsUnitNum     int64   `xorm:"int" json:"goods_unit_num"`        // 进货单单位数量
-	GoodsSingleNum   int64   `xorm:"int" json:"goods_single_num"`      // 进货单单件数量
-	GoodsUnitPrice   float64 `xorm:"double" json:"goods_unit_price"`   // 进货单位进价
-	GoodsSinglePrice float64 `xorm:"double" json:"goods_single_price"` // 进货单单件进价
-	OrderTotalPrice  float64 `xorm:"double" json:"goods_single_price"` // 该笔进货单总支出
-	IsDebt           bool    `json:"is_debt"`                          // 该笔订单是否赊账
+	OrderDetailId    int64   `xorm:"pk autoincr" json:"order_detail_id"` // 进货详情ID
+	OrderId          int64   `xorm:"bigint" json:"order_id"`             // 进货单ID
+	GoodsId          int64   `xorm:"int" json:"goods_id"`                // 商品ID
+	GoodsUnitNum     int64   `xorm:"int" json:"goods_unit_num"`          // 进货单单位数量
+	GoodsSingleNum   int64   `xorm:"int" json:"goods_single_num"`        // 进货单单件数量
+	GoodsUnitPrice   float64 `xorm:"double" json:"goods_unit_price"`     // 进货单位进价
+	GoodsSinglePrice float64 `xorm:"double" json:"goods_single_price"`   // 进货单单件进价
+	OrderTotalPrice  float64 `xorm:"double" json:"order_total_price"`    // 该笔进货单总支出
+	IsDebt           bool    `json:"is_debt"`                            // 该笔订单是否赊账
 }
